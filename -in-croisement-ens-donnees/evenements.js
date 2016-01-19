8587708b7c7c63a1d351149070dbd9ed42e1f996
Interfacteur <gaetan@interfacteur.net>
2016-01-20 00:15:22 +0100

    test loaded file with regex

--- -in-croisement-ens-donnees/evenements.js
+++ -in-croisement-ens-donnees/evenements.js
@@ -10,7 +10,20 @@
 		message = "Les données chargées ne correspondraient pas à la forme attendue",
 		fermeture = $(".fermeture").get(0)
 			.outerHTML
-			.replace(/(href="#)[^"]+"/, '$1javascript:void(0);"');
+			.replace(/(href="#)[^"]+"/, '$1javascript:void(0);"'),
+		clearLineJS = function (str) {
+			return str.replace(/\/\/.*/g, "") //sans les commentaires sur une ligne
+				.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //sans les commentaires sur plusieurs lignes
+				.replace(/^\s*/, "") //sans les espacements initiaux (dont retours)
+				.replace(/[\t\v]/g, " ") //remplacer les tabulations horizontales et verticales par un espacement
+				.replace(/\x20{2,}/g, " ") //remplacer les espacements multiples par un espacement
+				.replace(/[\f\n\r\u2028\u2029]/g, ""); //sans les retours à la page, à la ligne et charriot
+		},
+		re = {
+			informations: /var informations ?= ?{(?=.*"titre" ?: ?(?:(?:"(?:[^"]|\\")*[^\\]")|(?:'(?:[^']|\\')*[^\\]')))(?=.*"typeTableaux" ?: ?\[ ?(?:(?:(?:"(?:[^"]|\\")*[^\\]")|(?:'(?:[^']|\\')*[^\\]'))(?:(?: ?, ?(?=["']))|(?: ?))){3} ?\])(?=.*"genreTableaux" ?: ?(["'])[mf]\1)(?=.*"typeElements" ?: ?\[ ?(?:(?:(?:"(?:[^"]|\\")*[^\\]")|(?:'(?:[^']|\\')*[^\\]'))(?:(?: ?, ?(?=["']))|(?: ?))){2} ?\])(?=.*"genreElements" ?: ?(["'])[mf]\2)/,
+			datas: /var datas ?= ?\{ ?(?: ?"[^"]+" ?: ?\{ ?"titre" ?: ?(?:(?:"(?:[^"]|\\")*[^\\]")|(?:'(?:[^']|\\')*[^\\]')) ?, ?(?:"\d+" ?: ?\d+ ?(?:(?:, ?(?=(?:"\d)|\}))|(?=\}))){1,} ?\} ?(?:(?:, ?(?=["}]))|(?=\}))){2,7}\}/
+		};
+
 
 
 
@@ -46,12 +59,16 @@
 			.appendTo($b);
 		charge.onload = function() {
 			try {
-				var verification = this.result.replace(/^\s*|\s*$/g, "")
-				.replace(/[\t\n\r]|(  )/g,"");
-				console.log(verification);
+				var verification = clearLineJS(this.result),
+					erreur = [];
+				for (var k in re)
+					! re[k].test(verification)
+					&& erreur.push("la variable '" + k + "'");
+				if (erreur.length > 0)
+					throw "vérifier " + erreur.join(" et ");
 				eval(this.result);
-				$f.removeClass("settled");
 				$s.attr("src", "data:text/javascript;base64," + btoa(unescape(encodeURIComponent(this.result)))); //https://developer.mozilla.org/fr/docs/Décoder_encoder_en_base64
+				$f.removeClass("settled");
 			}
 			catch(e) {
 				$b.append($("<p>", {
