52adc66618c4e63709584f48cc9d6d099e91c53b
Interfacteur <gaetan@interfacteur.net>
2016-02-13 17:26:12 +0100

    to do

---
+++ -in-croisement-ens-donnees/_ref150211/0-todo.js
@@ -0,0 +1,39 @@
+/*
+13 février 2016
+
+FONCTIONNALITES
+
+to do : avec d'autres données que numériques
+
+to do : que faire quand partage le plus important == partage le moins important ?
+
+	done : analyse des croisements les plus riches entre deux tableaux
+	comme un bouton cliquable plus que comme une analyse
+
+	done : analyse du croisement minimal entre deux tableaux
+
+ACCESSIBILITÉ
+
+to do : couche ARIA ?
+
+to do : tester Voice Over ou NVDA : .on("keyup",
+	cf. this.$lien.on("mouseover keyup", Visu.passer);
+
+COMPATIBILITÉ
+
+to do : tests sur MSIE
+
+DÉVELOPPEMENT
+
+to do : reprendre en orientation prototypale, ou orienté objet ES6 ?
+	- 3-evenements.js OK
+
+to do : $c.remove(); //to do: detach() pour tous cas semblables ?
+
+PRESENTATION
+
+to do : largeur tableau quand excédée par largeur de #intersection ; &  l * 7.7 + .1 sur Chrome
+
+	done : effets de transition (notamment sur Chrome ?)
+
+*/
